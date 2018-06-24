1f08c52c52acd0ed73672a78bb29c58ba7794a6d
Denzel Narain <denzelnarain@gmail.com>
2018-06-24 16:52:13 +0200

    code edit

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
 
-  // Begin accessing JSON data here
+ 
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
